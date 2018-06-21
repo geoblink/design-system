9283e6deffffa9642c8bc2d7c1fa8967021d48d4
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2018-06-21 19:02:56 +0200

    :fire: Remove example components

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,8 @@
-import Vue from "vue"
-import Router from "vue-router"
-import Index from "@/templates/Index"
-import NotFound from "@/templates/NotFound"
+import Vue from 'vue'
+import Router from 'vue-router'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "Index",
-      component: Index,
-    },
-    {
-      path: "*",
-      name: "NotFound",
-      component: NotFound,
-    },
-  ],
+  routes: []
 })
