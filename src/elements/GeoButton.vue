40b0c5d1438d79a1e19c27ac3e635116e07296ba
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2018-07-18 12:25:18 +0200

    :recycle: Move geo-button documentation to separate file

--- src/elements/GeoButton.vue
+++ src/elements/GeoButton.vue
@@ -87,38 +87,3 @@ export default {
   }
 }
 </script>
-
-<docs>
-  ```jsx
-  <div class="element-demo">
-    <h3 class="element-demo__header">Regular</h3>
-    <div class="element-demo__block">
-      <geo-button type="primary">Primary</geo-button>
-      <geo-button type="secondary">Secondary</geo-button>
-      <geo-button type="tertiary">Tertiary</geo-button>
-      <geo-button type="destructive">Destructive</geo-button>
-    </div>
-    <h3 class="element-demo__header">Hover / Focus</h3>
-    <div class="element-demo__block">
-      <geo-button hover type="primary">Primary</geo-button>
-      <geo-button hover type="secondary">Secondary</geo-button>
-      <geo-button hover type="tertiary">Tertiary</geo-button>
-      <geo-button hover type="destructive">Destructive</geo-button>
-    </div>
-    <h3 class="element-demo__header">Disabled</h3>
-    <div class="element-demo__block">
-      <geo-button disabled type="primary">Primary</geo-button>
-      <geo-button disabled type="secondary">Secondary</geo-button>
-      <geo-button disabled type="tertiary">Tertiary</geo-button>
-      <geo-button disabled type="destructive">Destructive</geo-button>
-    </div>
-    <h3 class="element-demo__header">Disabled &amp; Hover / Focus</h3>
-    <div class="element-demo__block">
-      <geo-button disabled hover type="primary">Primary</geo-button>
-      <geo-button disabled hover type="secondary">Secondary</geo-button>
-      <geo-button disabled hover type="tertiary">Tertiary</geo-button>
-      <geo-button disabled hover type="destructive">Destructive</geo-button>
-    </div>
-  </div>
-  ```
-</docs>
