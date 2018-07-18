b29cd56837ab68c0a652090508079cff2957ad06
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2018-07-18 12:22:42 +0200

    :heavy_plus_sign: Add Fontawesome to demo

--- docs/docs.helper.js
+++ docs/docs.helper.js
@@ -7,5 +7,14 @@ import WebFontLoader from '../src/utils/webFontLoader' // eslint-disable-line no
 import statusLabels from './utils/statusLabels'
 import activeNav from './utils/activeNav'
 
+import fontawesome from '@fortawesome/fontawesome'
+import faFreeBrands from '@fortawesome/fontawesome-free-brands'
+import faFreeSolid from '@fortawesome/fontawesome-free-solid'
+import faFreeRegular from '@fortawesome/fontawesome-free-regular'
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.mixin(statusLabels)
 Vue.mixin(activeNav)
+
+fontawesome.library.add(faFreeBrands, faFreeSolid, faFreeRegular)
