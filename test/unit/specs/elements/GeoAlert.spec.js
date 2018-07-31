248130812cb45204914759df9b6d21c94085d529
Ricardo Boluda <rboluda@geoblink.com>
2018-07-31 16:39:55 +0200

    :white_check_mark: Add tests for GeoAlert

---
+++ test/unit/specs/elements/GeoAlert.spec.js
@@ -0,0 +1,77 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import GeoAlert from '@/elements/GeoAlert.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fab, fas, far)
+// create an extended `Vue` constructor
+const localVue = createLocalVue()
+localVue.component('geo-alert', GeoAlert)
+
+describe('GeoAlert', () => {
+  it('should render activity indicator', function () {
+    const wrapper = mount(GeoAlert, {
+      propsData: {
+        variant: 'success'
+      }
+    })
+
+    expect(wrapper.find('.geo-alert').exists()).toBe(true)
+  })
+
+  it('should show the close button if close listener is provided', function () {
+    const wrapper = mount(GeoAlert, {
+      propsData: {
+        variant: 'success',
+        closeIcon: ['fas', 'times']
+      },
+      listeners: {
+        close: function () {}
+      }
+    })
+    expect(wrapper.find('.geo-alert').exists()).toBe(true)
+    expect(wrapper.find('.geo-alert__content__close-icon').exists()).toBe(true)
+    expect(wrapper.vm.shouldShowCloseButton).toBe(true)
+  })
+
+  it('shouldn\'t show the close button if close listener is not provided', function () {
+    const wrapper = mount(GeoAlert, {
+      propsData: {
+        variant: 'success'
+      }
+    })
+    expect(wrapper.find('.geo-alert').exists()).toBe(true)
+    expect(wrapper.find('.geo-alert__content__close-icon').exists()).toBe(false)
+    expect(wrapper.vm.shouldShowCloseButton).toBe(false)
+  })
+
+  it('Should emit a close event when clicking on the close button', function () {
+    const wrapper = mount(GeoAlert, {
+      propsData: {
+        variant: 'success',
+        closeIcon: ['fas', 'times']
+      },
+      listeners: {
+        close: function () {}
+      }
+    })
+    wrapper.find('.geo-alert__content__close-icon').trigger('click')
+    expect(wrapper.emitted().close).toBeTruthy()
+  })
+
+  it('Should apply a CSS suffix when the modifier is provided', function () {
+    const wrapper = mount(GeoAlert, {
+      propsData: {
+        variant: 'success',
+        closeIcon: ['fas', 'times'],
+        cssModifier: 'test-alert'
+      },
+      listeners: {
+        close: function () {}
+      }
+    })
+    expect(wrapper.find('.geo-alert__content__close-icon--test-alert').exists()).toBe(true)
+  })
+})
