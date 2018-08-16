4507ea41386477a20891c10b0cbefc55846b638d
Ricardo Boluda <rboluda@geoblink.com>
2018-08-16 16:32:24 +0200

    :tada: Create component files

---
+++ src/elements/GeoSelect.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Something</div>
+</template>
+
+<script>
+module.exports = {
+  name: 'geoSelect',
+  status: 'ready',
+  version: '1.0.1',
+  data () {},
+  computed: {},
+  methods: {}
+}
+</script>
+
