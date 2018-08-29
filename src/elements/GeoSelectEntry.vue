7fd5b5bd1e67d428d4d1a7ce22459471b77c6edb
Ricardo Boluda <rboluda@geoblink.com>
2018-08-29 14:20:01 +0200

    Add geo select entry component

---
+++ src/elements/GeoSelectEntry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="`geo-select-entry__container${cssSuffix}`">
+    <slot name="leftAccessoryItem" />
+    <slot name="content" />
+    <slot name="rightAccessoryItem" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GeoSelectEntry',
+  status: 'ready',
+  version: '1.0.1',
+  props: {
+    /**
+     * An optional suffix to be appended as BEM modifier.
+     *
+     * Can be used to customize the look & feel of the component by changing all
+     * the CSS classes by different ones so no CSS loaded by default affects
+     * them.
+     *
+     * To generate default styles for a modifier named `modifier-name`, you just
+     * have to add `@include geo-activity-indicator-make('modifier-name');` to
+     * your SCSS styles.
+     */
+    cssModifier: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    cssSuffix () {
+      return this.cssModifier ? `--${this.cssModifier}` : ''
+    }
+  }
+}
+</script>
+
