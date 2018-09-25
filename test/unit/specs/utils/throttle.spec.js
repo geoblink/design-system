a972410ce5f61ad73793c655bdddf59c193ab027
David Del Prado <ddelprado@geoblink.com>
2018-09-25 12:05:02 +0200

    Improved throttle tests

--- test/unit/specs/utils/throttle.spec.js
+++ test/unit/specs/utils/throttle.spec.js
@@ -1,32 +1,30 @@
 import throttle from 'src/utils/throttle'
 
-jest.useFakeTimers()
-
-let throttleCallback
-let mockFn
-
-beforeEach(() => {
-  mockFn = jest.fn()
-  throttleCallback = throttle(mockFn)
-})
-
 describe('throttle.js', () => {
+  const mockFn = jest.fn()
+  const throttleCallback = throttle(mockFn)
+
+  beforeEach(() => {
+    mockFn.mockClear()
+  })
+
   it('should return a function', () => {
     expect(throttleCallback).toEqual(expect.any(Function))
   })
 
-  it('should not be called more than 1 time', () => {
+  it('should be called once when run consecutively', () => {
     throttleCallback()
     throttleCallback()
 
     expect(mockFn).toHaveBeenCalledTimes(1)
   })
 
-  it('should be called more than 1 time if timeout triggers', () => {
+  it('should be called more than once when there is some delay between calls', (done) => {
     throttleCallback()
-    jest.runAllTimers()
-    throttleCallback()
-
-    expect(mockFn).toHaveBeenCalledTimes(2)
+    setTimeout(function () {
+      throttleCallback()
+      expect(mockFn).toHaveBeenCalledTimes(2)
+      done()
+    }, 11)
   })
 })
