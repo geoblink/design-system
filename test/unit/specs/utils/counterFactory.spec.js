c3a396204322f2fb65fb98055baf714d8391b232
David Del Prado <ddelprado@geoblink.com>
2018-09-25 10:54:46 +0200

    Improvements to counterFactory tests

--- test/unit/specs/utils/counterFactory.spec.js
+++ test/unit/specs/utils/counterFactory.spec.js
@@ -1,29 +1,23 @@
 import counterFactory from 'src/utils/counterFactory.js'
 
-let counter
-
-beforeEach(() => {
-  counter = counterFactory()
-})
-
 describe('counterFactory.js', () => {
+  let counter
+
+  beforeEach(() => {
+    counter = counterFactory()
+  })
+
   it('should return a function', () => {
     expect(counter).toEqual(expect.any(Function))
   })
 
   it('should return 0 on first tick', () => {
-    const counterValue = counter()
-
-    expect(counterValue).toBe(0)
+    expect(counter()).toBe(0)
   })
 
   it('should increase counter on 1 on each tick', () => {
-    let counterValue = counter()
-
-    expect(counterValue).toBe(0)
-    counterValue = counter()
-    expect(counterValue).toBe(1)
-    counterValue = counter()
-    expect(counterValue).toBe(2)
+    expect(counter()).toBe(0)
+    expect(counter()).toBe(1)
+    expect(counter()).toBe(2)
   })
 })
