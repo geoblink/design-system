f63a5d082e48b9c1f2e2969472c8816f46d85d02
David Del Prado <ddelprado@geoblink.com>
2018-09-25 11:04:53 +0200

    Reducing scope of variables

--- test/unit/specs/utils/getDOMElementOffset.spec.js
+++ test/unit/specs/utils/getDOMElementOffset.spec.js
@@ -2,16 +2,16 @@ import getDOMElementOffset from 'src/utils/getDOMElementOffset'
 import GeoButton from '@/elements/GeoButton/GeoButton.vue'
 import { mount } from '@vue/test-utils'
 
-const getBoundingClientRectMock = function () {
-  return {
-    top: 100,
-    left: 200
-  }
-}
-const wrapper = mount(GeoButton, { propsData: { type: 'primary' } })
-wrapper.getBoundingClientRect = jest.fn(getBoundingClientRectMock)
-
 describe('getDOMElementOffset.js', () => {
+  const getBoundingClientRectMock = function () {
+    return {
+      top: 100,
+      left: 200
+    }
+  }
+  const wrapper = mount(GeoButton, { propsData: { type: 'primary' } })
+  wrapper.getBoundingClientRect = jest.fn(getBoundingClientRectMock)
+
   it('should call element\'s getBoundingClientRect', () => {
     getDOMElementOffset(wrapper)
     expect(wrapper.getBoundingClientRect).toHaveBeenCalled()
