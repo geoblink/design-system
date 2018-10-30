e47858cc4a6bb3a04f38315a62e81fc8d231a7a2
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2018-10-30 14:34:55 +0100

    :sparkles: Add `GeoBorderedBox` components
    and deprecated old `GeoDropdownFooter` and `GeoDropdownHeader`
    components

---
+++ src/elements/GeoBorderedBox/GeoBorderedBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="`geo-bordered-box${cssSuffix}`">
+    <div class="geo-bordered-box__header">
+      <slot name="header" />
+    </div>
+    <div class="geo-bordered-box__body">
+      <slot />
+    </div>
+    <div class="geo-bordered-box__footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+import cssSuffix from '../../mixins/cssModifierMixin'
+
+export default {
+  name: 'GeoBorderedBox',
+  status: 'missing-tests',
+  release: '7.3.0',
+  mixins: [cssSuffix],
+  props: {},
+  computed: {
+    hasHeader () {
+      return !!this.slots.header.length
+    },
+
+    hasFooter () {
+      return !!this.slots.footer.length
+    }
+  }
+}
+</script>
---
+++ test/unit/specs/elements/GeoBorderedBox/GeoBorderedBoxFooter.spec.js
@@ -0,0 +1,18 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import GeoBorderedBoxFooter from '@/elements/GeoBorderedBox/GeoBorderedBoxFooter.vue'
+
+// create an extended `Vue` constructor
+const localVue = createLocalVue()
+localVue.component('geo-bordered-box-footer', GeoBorderedBoxFooter)
+
+describe('GeoBorderedBoxFooter', () => {
+  it('should render default slot', function () {
+    const wrapper = mount(GeoBorderedBoxFooter, {
+      slots: {
+        default: [`<span class="my-demo-content">Just some unique demo content</span>`]
+      }
+    })
+
+    expect(wrapper.find('.my-demo-content').exists()).toBe(true)
+  })
+})
