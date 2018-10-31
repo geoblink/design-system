0a95c16257d737e2e5dc48796b16a9081586ce91
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2018-10-31 09:42:14 +0100

    :sparkles: Add `GeoListClearItem` and `GeoListFooterButton` components
    and deprecate `GeoSelectMoreResultsFooter` and `GeoSelectReadOnlyEntry`

---
+++ src/elements/GeoList/GeoListClearItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="`geo-list-clear-item${cssSuffix}`">
+    <!-- @slot Use this slot to customize content displayed in this entry -->
+    <slot />
+  </div>
+</template>
+
+<script>
+import cssSuffix from '../../mixins/cssModifierMixin'
+
+export default {
+  name: 'GeoListClearItem',
+  status: 'missing-tests',
+  release: '8.0.0',
+  mixins: [cssSuffix]
+}
+</script>
---
+++ src/elements/GeoList/GeoListFooterButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="`geo-list-footer-button${cssSuffix}`">
+    <div
+      class="geo-list-footer-button__button"
+      @click="handleClick($event)"
+    >
+      <span class="geo-list-footer-button__label">
+        <!-- @slot Use this slot to customize the label of the button -->
+        <slot />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import cssSuffix from '../../mixins/cssModifierMixin'
+
+export default {
+  name: 'GeoListFooterButton',
+  status: 'missing-tests',
+  release: '8.0.0',
+  mixins: [cssSuffix],
+  methods: {
+    handleClick ($event) {
+      /**
+       * User clicked in this button
+       *
+       * @event click
+       * @type {MouseEvent}
+       */
+      this.$emit('click', $event)
+    }
+  }
+}
+</script>
