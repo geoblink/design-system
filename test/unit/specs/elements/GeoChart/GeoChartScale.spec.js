bec8f0c530cb48cdc42953206642fcacef4db56c
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2019-01-22 12:52:37 +0100

    :white_check_mark: Add tests to logarithmic and categorical scales

--- test/unit/specs/elements/GeoChart/GeoChartScale.spec.js
+++ test/unit/specs/elements/GeoChart/GeoChartScale.spec.js
@@ -65,70 +65,7 @@ describe('GeoChartScale', function () {
         expect(Object.getPrototypeOf(scale.axisScale)).toBe(Object.getPrototypeOf(d3.scaleLinear))
       })
 
-      it('should consider chart width in range of horizontal axis', function () {
-        const width = 200
-        const height = 100
-
-        const scale = GeoChartScale.getNewScale(
-          linearScaleBaseConfig,
-          _.merge({}, chartBaseConfig, { size: { width, height } })
-        )
-
-        expect(scale.axisScale.range()).toEqual([0, width])
-      })
-
-      it('should consider chart margin in range of horizontal axis', function () {
-        const leftMargin = 100
-        const rightMargin = 50
-        const width = 200
-        const margin = {
-          top: 30,
-          right: rightMargin,
-          bottom: 90,
-          left: leftMargin
-        }
-
-        const scale = GeoChartScale.getNewScale(
-          linearScaleBaseConfig,
-          _.merge({}, chartBaseConfig, { margin, size: { width } })
-        )
-
-        expect(scale.axisScale.range()).toEqual([leftMargin, width - rightMargin])
-      })
-
-      it('should consider chart height in range of vertical axis', function () {
-        const dimension = GeoChartAxis.DIMENSIONS.vertical
-        const width = 200
-        const height = 100
-
-        const scale = GeoChartScale.getNewScale(
-          _.merge(linearScaleBaseConfig, { dimension }),
-          _.merge({}, chartBaseConfig, { size: { width, height } })
-        )
-
-        expect(scale.axisScale.range()).toEqual([0, height])
-      })
-
-      it('should consider chart margin in range of vertical axis', function () {
-        const dimension = GeoChartAxis.DIMENSIONS.vertical
-        const topMargin = 75
-        const bottomMargin = 50
-        const width = 200
-        const height = 300
-        const margin = {
-          top: topMargin,
-          right: 60,
-          bottom: bottomMargin,
-          left: 40
-        }
-
-        const scale = GeoChartScale.getNewScale(
-          _.merge({}, linearScaleBaseConfig, { dimension }),
-          _.merge({}, chartBaseConfig, { margin, size: { width, height } })
-        )
-
-        expect(scale.axisScale.range()).toEqual([topMargin, height - bottomMargin])
-      })
+      testRange(linearScaleBaseConfig)
 
       it('should set domain using start and end', function () {
         const domain = {
@@ -181,5 +118,227 @@ describe('GeoChartScale', function () {
         }).toThrow()
       })
     })
+
+    describe('Logarithmic scales', function () {
+      const logarithmicScaleBaseConfig = {
+        id: 0,
+        dimension: GeoChartAxis.DIMENSIONS.horizontal,
+        scale: {
+          type: GeoChartScale.SCALE_TYPES.logarithmic,
+          valueForOrigin: 0,
+          domain: {
+            start: 10,
+            end: 100
+          }
+        }
+      }
+
+      it('should return a valid scale', function () {
+        const valueForOrigin = 20
+
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, logarithmicScaleBaseConfig, { scale: { valueForOrigin } }),
+          chartBaseConfig
+        )
+
+        expect(scale).toHaveProperty('valueForOrigin', valueForOrigin)
+        expect(scale).toHaveProperty('axisScale')
+        expect(scale.axisScale).toBeInstanceOf(Function)
+        expect(Object.getPrototypeOf(scale.axisScale)).toBe(Object.getPrototypeOf(d3.scaleLog))
+      })
+
+      testRange(logarithmicScaleBaseConfig)
+
+      it('should set domain using start and end', function () {
+        const domain = {
+          start: 100,
+          end: 500
+        }
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, logarithmicScaleBaseConfig, { scale: { domain } }),
+          chartBaseConfig
+        )
+
+        expect(scale.axisScale.domain()).toEqual([domain.start, domain.end])
+      })
+
+      it('should set domain using list of numbers', function () {
+        const domainStart = 100
+        const domainMiddle = 150
+        const domainEnd = 500
+        const domain = [domainStart, domainEnd]
+        const domainWithMiddle = [domainStart, domainMiddle, domainEnd]
+
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, logarithmicScaleBaseConfig, { scale: { domain } }),
+          chartBaseConfig
+        )
+
+        expect(scale.axisScale.domain()).toEqual(domain)
+
+        const scaleWithMiddle = GeoChartScale.getNewScale(
+          _.merge({}, logarithmicScaleBaseConfig, { scale: { domain: domainWithMiddle } }),
+          chartBaseConfig
+        )
+
+        expect(scaleWithMiddle.axisScale.domain()).toEqual(domainWithMiddle)
+      })
+
+      it('should throw error when using an invalid domain', function () {
+        const domain = {
+          start: 100
+        }
+
+        expect(function () {
+          const config = _.cloneDeep(logarithmicScaleBaseConfig)
+          config.scale.domain = domain
+
+          return GeoChartScale.getNewScale(
+            config,
+            chartBaseConfig
+          )
+        }).toThrow()
+      })
+    })
+
+    describe('Categorical scales', function () {
+      const categoricalScaleBaseConfig = {
+        id: 0,
+        dimension: GeoChartAxis.DIMENSIONS.horizontal,
+        scale: {
+          type: GeoChartScale.SCALE_TYPES.categorical,
+          valueForOrigin: 0,
+          domain: {
+            start: 10,
+            end: 100
+          }
+        }
+      }
+
+      it('should return a valid scale', function () {
+        const valueForOrigin = 20
+
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, categoricalScaleBaseConfig, { scale: { valueForOrigin } }),
+          chartBaseConfig
+        )
+
+        expect(scale).toHaveProperty('valueForOrigin', valueForOrigin)
+        expect(scale).toHaveProperty('axisScale')
+        expect(scale.axisScale).toBeInstanceOf(Function)
+        expect(Object.getPrototypeOf(scale.axisScale)).toBe(Object.getPrototypeOf(d3.scaleLog))
+      })
+
+      testRange(categoricalScaleBaseConfig)
+
+      it('should set domain using list of strings', function () {
+        const categoryA = 100
+        const categoryB = 150
+        const categoryC = 500
+        const domainWith2Items = [categoryA, categoryC]
+        const domainWith3Items = [categoryA, categoryB, categoryC]
+
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, categoricalScaleBaseConfig, { scale: { domain: domainWith2Items } }),
+          chartBaseConfig
+        )
+
+        expect(scale.axisScale.domain()).toEqual(domainWith2Items)
+
+        const scaleWithMiddle = GeoChartScale.getNewScale(
+          _.merge({}, categoricalScaleBaseConfig, { scale: { domain: domainWith3Items } }),
+          chartBaseConfig
+        )
+
+        expect(scaleWithMiddle.axisScale.domain()).toEqual(domainWith3Items)
+      })
+
+      it('should set inner padding', function () {
+        const innerPadding = 0.4
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, categoricalScaleBaseConfig, { scale: { padding: { inner: innerPadding } } }),
+          chartBaseConfig
+        )
+
+        expect(scale.axisScale.paddingInner()).toEqual(innerPadding)
+      })
+
+      it('should set outer padding', function () {
+        const outerPadding = 0.2
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, categoricalScaleBaseConfig, { scale: { padding: { outer: outerPadding } } }),
+          chartBaseConfig
+        )
+
+        expect(scale.axisScale.paddingOuter()).toEqual(outerPadding)
+      })
+    })
+
+    function testRange (scaleConfig) {
+      it('should consider chart width in range of horizontal axis', function () {
+        const width = 200
+        const height = 100
+
+        const scale = GeoChartScale.getNewScale(
+          scaleConfig,
+          _.merge({}, chartBaseConfig, { size: { width, height } })
+        )
+
+        expect(scale.axisScale.range()).toEqual([0, width])
+      })
+
+      it('should consider chart margin in range of horizontal axis', function () {
+        const leftMargin = 100
+        const rightMargin = 50
+        const width = 200
+        const margin = {
+          top: 30,
+          right: rightMargin,
+          bottom: 90,
+          left: leftMargin
+        }
+
+        const scale = GeoChartScale.getNewScale(
+          scaleConfig,
+          _.merge({}, chartBaseConfig, { margin, size: { width } })
+        )
+
+        expect(scale.axisScale.range()).toEqual([leftMargin, width - rightMargin])
+      })
+
+      it('should consider chart height in range of vertical axis', function () {
+        const dimension = GeoChartAxis.DIMENSIONS.vertical
+        const width = 200
+        const height = 100
+
+        const scale = GeoChartScale.getNewScale(
+          _.merge(scaleConfig, { dimension }),
+          _.merge({}, chartBaseConfig, { size: { width, height } })
+        )
+
+        expect(scale.axisScale.range()).toEqual([0, height])
+      })
+
+      it('should consider chart margin in range of vertical axis', function () {
+        const dimension = GeoChartAxis.DIMENSIONS.vertical
+        const topMargin = 75
+        const bottomMargin = 50
+        const width = 200
+        const height = 300
+        const margin = {
+          top: topMargin,
+          right: 60,
+          bottom: bottomMargin,
+          left: 40
+        }
+
+        const scale = GeoChartScale.getNewScale(
+          _.merge({}, scaleConfig, { dimension }),
+          _.merge({}, chartBaseConfig, { margin, size: { width, height } })
+        )
+
+        expect(scale.axisScale.range()).toEqual([topMargin, height - bottomMargin])
+      })
+    }
   })
 })
