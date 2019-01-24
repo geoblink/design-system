e98e7f954baf4cf87c312b7778c06bfab6c9317c
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2019-01-24 15:19:27 +0100

    :fire: [GeoChart] Remove unused test code

--- test/unit/specs/elements/GeoChart/GeoChart.spec.js
+++ test/unit/specs/elements/GeoChart/GeoChart.spec.js
@@ -1,12 +1,8 @@
-import { createLocalVue, mount } from '@vue/test-utils'
 import GeoChart from '@/elements/GeoChart/GeoChart.vue'
 
 import * as GeoChartScale from '@/elements/GeoChart/GeoChartScale'
 import * as GeoChartAxis from '@/elements/GeoChart/GeoChartAxis'
 import * as GeoChartBars from '@/elements/GeoChart/GeoChartBars'
-
-const localVue = createLocalVue()
-localVue.component('geo-chart', GeoChart)
 
 describe('GeoChart', () => {
   describe('Constants', function () {
