c2039248b320880c6db86708f369e40e0c7e6c6c
David Del Prado <ddelprado@geoblink.com>
2019-02-26 08:12:03 +0100

    animations

--- src/elements/GeoChart/GeoChartTextDescription.js
+++ src/elements/GeoChart/GeoChartTextDescription.js
@@ -39,7 +39,7 @@ export function setupTextDescriptions (settingsData, d3Instance, globalOptions) 
   function getCSSClassesGroups (options, i) {
     const defaultGroupCSSClasses = ['geo-chart-text-descriptions']
 
-    const customCSSClasses = _.isFunction(options.cssClasses)
+    const customCSSClasses = _.isFunction(options.cssClassesGroups)
       ? options.cssClassesGroups(defaultGroupCSSClasses)
       : defaultGroupCSSClasses
 
@@ -49,6 +49,7 @@ export function setupTextDescriptions (settingsData, d3Instance, globalOptions) 
 
 function renderSingleGroup (group, singleOptions, globalOptions) {
   const textOptions = singleOptions.textOptions
+  const margin = _.get(singleOptions.textOptions, 'margin', 0)
   const textGroups = group
     .selectAll('text')
     .data(singleOptions.data)
@@ -56,13 +57,20 @@ function renderSingleGroup (group, singleOptions, globalOptions) {
   const newTextGroups = textGroups
     .enter()
     .append('text')
-    .attr('class', 'TODO-getclass')
-    .attr('dominant-baseline', 'central')
+    .attr('class', getCSSClassesTexts)
+    .attr('dominant-baseline', 'text-before-edge')
     .attr('text-anchor', singleOptions.textAnchor)
+    .attr('y', 0)
+
+  newTextGroups
+    .transition()
+    .duration(globalOptions.chart.animationsDurationInMilliseconds)
     .attr('y', singleOptions.getTextPositionMainDirection)
 
   const updatedTextGroups = textGroups
   updatedTextGroups
+    .transition()
+    .duration(globalOptions.chart.animationsDurationInMilliseconds)
     .attr('text-anchor', singleOptions.textAnchor)
     .attr('y', singleOptions.getTextPositionMainDirection)
 
@@ -80,17 +88,19 @@ function renderSingleGroup (group, singleOptions, globalOptions) {
 
   var heights = []
   var preferredPositions = []
-  allTextGroups.each(function () {
+  allTextGroups.each(function (d) {
     heights.push(this.getBBox().height)
     preferredPositions.push(this.getBBox().y)
+    console.log(d.data.value)
+    console.log(this.getBBox())
   })
 
-  console.log(heights)
-  console.log(preferredPositions)
-  var positions = computeLabelPositions(heights, preferredPositions, 0, singleOptions.range[1], singleOptions.range[0])
-  console.log(positions)
+  // console.log(heights)
+  // console.log(preferredPositions)
+  // var positions = computeLabelPositions(heights, preferredPositions, margin, singleOptions.range[1], singleOptions.range[0])
+  // console.log(positions)
 
-  setTextElementsGroupsPosition(allTextGroups, positions)
+  // setTextElementsGroupsPosition(allTextGroups, positions)
 
   function setTextContent (groups) {
     const tspans = groups
@@ -102,13 +112,18 @@ function renderSingleGroup (group, singleOptions, globalOptions) {
     const newtspans = tspans
       .enter()
       .append('tspan')
-      .attr('class', 'TODO-getlcass')
+      .attr('class', function (d) {
+        return d.cssClass
+      })
       .text(function (d) {
         return d.text
       })
 
     const updatedtspans = tspans
     updatedtspans
+      .attr('class', function (d) {
+        return d.cssClass
+      })
       .text(function (textD) {
         return textD.text
       })
@@ -140,74 +155,20 @@ function renderSingleGroup (group, singleOptions, globalOptions) {
         var y = positions[i]
         // textGroup.select('text')
         // .attr('transform', `translate(${xOffset}, ${y + singleOptions.margin})`)
-        // textGroup.attr('y', y)
+        textGroup.attr('y', y)
       } else {
         // textGroup.remove()
       }
     })
   }
-}
 
-/**
- * @param {Array<Number>} heights
- * @param {Array<Number>} preferredPositions
- * @param {Number} textGroupMargin
- * @param {Number} maxY
- * @param {Number} minY
- * @returns {Array<Number>} positions
- */
+  function getCSSClassesTexts (options, i) {
+    const defaultCSSClasses = ['geo-chart-text-descriptions__text']
 
-function computeLabelPositions (heights, preferredPositions, textGroupMargin, maxY, minY) {
-  var positions = []
-  var initialMaxY = maxY
-  for (var i = 0; i < heights.length; i++) {
-    var heightWithMargin = heights[i] + 2 * textGroupMargin
-    var lowerY = preferredPositions[i] - heightWithMargin / 2
-    var upperY = lowerY + heightWithMargin
-    if (upperY > maxY) {
-      lowerY = maxY - heightWithMargin
-      upperY = lowerY + heightWithMargin
-    }
-    if (lowerY < minY) {
-      lowerY = minY
-      upperY = lowerY + heightWithMargin
-      var tentativeChangePositions = [] // use alternative array in case it is not possible to relocate
-      var isBackFixed = false // fix backwards if we can
-      var j = i
-      tentativeChangePositions[j] = minY // position has to be at least minY
-      for (j = i - 1; j >= 0; j--) {
-        var currentPosition = positions[j]
-        var backPressurePosition = tentativeChangePositions[j + 1]
-        var backPressureHeightWithMargin = heights[j + 1] + 2 * textGroupMargin
-        var upperBackPressureY = backPressurePosition + backPressureHeightWithMargin
-        if (upperBackPressureY < currentPosition) {
-          isBackFixed = true
-          break
-        } else {
-          tentativeChangePositions[j] = upperBackPressureY
-        }
-      }
-      // We moved everything and last one is correct
-      if (j === -1 && !isBackFixed) {
-        var firstElementHeightWithMargin = heights[0] + 2 * textGroupMargin
-        if (tentativeChangePositions[0] + firstElementHeightWithMargin < initialMaxY) {
-          isBackFixed = true
-        }
-      }
+    const customCSSClasses = _.isFunction(options.cssClassesTexts)
+      ? options.cssClassesTexts(defaultCSSClasses)
+      : defaultCSSClasses
 
-      if (isBackFixed) {
-        // copy tentative possitions since it was successful
-        // j is the index where we checked everything is good, so don't need to change j
-        for (var k = j + 1; k <= i; k++) {
-          positions[k] = tentativeChangePositions[k]
-        }
-      } else {
-        // bail out we don't set anything further
-        return positions
-      }
-    }
-    maxY = lowerY
-    positions[i] = lowerY
+    return customCSSClasses
   }
-  return positions
 }
