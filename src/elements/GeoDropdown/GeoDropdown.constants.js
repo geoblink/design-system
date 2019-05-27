2cfe30b451efc4363d027d9e9bde2b38001abea8
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2019-05-27 10:26:34 +0200

    :sparkles: Add mixin to reduce `GeoDropdown` boilerplate

--- src/elements/GeoDropdown/GeoDropdown.constants.js
+++ src/elements/GeoDropdown/GeoDropdown.constants.js
@@ -1,11 +1,34 @@
-const X_AXIS_POSITION = {
+import _ from 'lodash'
+
+export const X_AXIS_POSITION = {
   right: 'right',
   left: 'left'
 }
 
-const Y_AXIS_POSITION = {
+export const Y_AXIS_POSITION = {
   top: 'top',
   bottom: 'bottom'
 }
 
-export { X_AXIS_POSITION, Y_AXIS_POSITION }
+export function geoDropdownMixinFactory (idDropdown) {
+  const camelCaseIdDropdown = _.camelCase(idDropdown)
+  const pascalCasedIdDropdown = `${camelCaseIdDropdown[0].toUpperCase()}${camelCaseIdDropdown.slice(1)}`
+  const isDropdownOpenedKeyPath = `is${pascalCasedIdDropdown}DropdownOpened`
+
+  return {
+    data () {
+      return {
+        [isDropdownOpenedKeyPath]: false
+      }
+    },
+    methods: {
+      [`dismiss${pascalCasedIdDropdown}Dropdown`] () {
+        (/** @type {any} */ (this))[isDropdownOpenedKeyPath] = false
+      },
+
+      [`toggle${pascalCasedIdDropdown}Dropdown`] () {
+        (/** @type {any} */ (this))[isDropdownOpenedKeyPath] = !this[isDropdownOpenedKeyPath]
+      }
+    }
+  }
+}
