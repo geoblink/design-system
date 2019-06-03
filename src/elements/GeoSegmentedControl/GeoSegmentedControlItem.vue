0048a509f688a6971d8f98713977b20a93e44d2e
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2019-06-03 11:57:23 +0200

    :sparkles: [GeoSegmentedControlItem] Add `variant` support
    and shortcut elements

--- src/elements/GeoSegmentedControl/GeoSegmentedControlItem.vue
+++ src/elements/GeoSegmentedControl/GeoSegmentedControlItem.vue
@@ -2,11 +2,15 @@
   <div
     :class="{
       [`geo-segmented-control-item${outlineSuffix}${cssSuffix}`]: true,
+      [`geo-segmented-control-item--${variant}${outlineSuffix}${cssSuffix}`]: true,
       [`geo-segmented-control-item${outlineSuffix}--active${cssSuffix}`]: active,
-      [`geo-segmented-control-item${outlineSuffix}--disabled${cssSuffix}`]: disabled
+      [`geo-segmented-control-item--${variant}${outlineSuffix}--active${cssSuffix}`]: active,
+      [`geo-segmented-control-item${outlineSuffix}--disabled${cssSuffix}`]: disabled,
+      [`geo-segmented-control-item--${variant}${outlineSuffix}--disabled${cssSuffix}`]: disabled
     }"
     @click="handleClick($event)"
   >
+    <!-- @slot Use this slot to customize item's content -->
     <slot />
   </div>
 </template>
@@ -14,37 +18,46 @@
 <script>
 import cssSuffix from '../../mixins/cssModifierMixin'
 
+import geoSegmentedControlItemMixin, { VARIANTS } from './GeoSegmentedControlItem.constants'
+
 export default {
   name: 'GeoSegmentedControlItem',
-  status: 'missing-tests',
+  status: 'ready',
   release: '6.2.0',
-  mixins: [cssSuffix],
+  mixins: [cssSuffix, geoSegmentedControlItemMixin],
   props: {
     /**
-     * Whether this item should be styled as active (`true`) or not (default).
-     */
-    active: {
-      type: Boolean,
-      default: false
-    },
-    /**
-     * Whether this item should be displayed using outline style (`true`) or not.
+     * Predefined color scheme of the segmented control item, allowing several
+     * common out-of-the-box customizations.
      *
-     * Outline style takes more vertical space while default style is more
-     * compact.
-     */
-    outline: {
-      type: Boolean,
-      default: false
-    },
-    /**
-     * Whether this item is disabled (`true`) or not be (default).
+     * > **Note:** There are specific components to avoid explicitly writing
+     * > this value and performing other common customizations.
      *
-     * Disabled items won't emit click events.
+     *
+     * | variant  | Specific component                                            |
+     * |----------|---------------------------------------------------------------|
+     * | success  | [GeoSuccessSegmentedControlItem](./#/Elements/GeoSegmentedControlItem?id=geosuccesssegmentedcontrolitem)   |
+     * | info     | [GeoInfoSegmentedControlItem](./#/Elements/GeoSegmentedControlItem?id=geoinfosegmentedcontrolitem)         |
+     * | warning  | [GeoWarningSegmentedControlItem](./#/Elements/GeoSegmentedControlItem?id=geowarningsegmentedcontrolitem)   |
+     * | error    | [GeoErrorSegmentedControlItem](./#/Elements/GeoSegmentedControlItem?id=geoerrorsegmentedcontrolitem)       |
+     *
+     * Supported `variant` values are exported under `VARIANTS` named export.
+     * See [Component Constants](./#/Component%20Constants) for more info on how
+     * to use those constants in your code.
+     *
+     * > **Note:** You can always override the color scheme of any
+     * > `GeoSegmentedControlItem` using `cssModifier` prop.
      */
-    disabled: {
-      type: Boolean,
-      default: false
+    variant: {
+      type: String,
+      default: VARIANTS.info,
+      validator (value) {
+        if (value in VARIANTS) return true
+
+        const supportedValues = Object.values(VARIANTS).map(i => `«${i}»`).join(', ')
+        console.warn(`GeoSegmentedControlItem [component] :: Unsupported value («${value}») for «variant» property. Use one of ${supportedValues}`)
+        return false
+      }
     }
   },
   computed: {
