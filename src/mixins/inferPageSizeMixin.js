317516bc90dc24b244097783d38a1d27d53a4511
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2019-07-24 07:47:12 +0200

    :recycle: [GeoTable] Move automatic page size inference algorithm to a mixin

---
+++ src/mixins/inferPageSizeMixin.js
@@ -0,0 +1,148 @@
+/**
+ * @readonly
+ * @enum {string}
+ */
+export const DATA_KEYS = {
+  isInferringPageSize: 'geoblinkDesignSystem_inferPageSizeMixin_isInferringPageSize',
+  internallyForcedCurrentPageStart: 'geoblinkDesignSystem_inferPageSizeMixin_internallyForcedCurrentPageStart',
+  inferredPageSize: 'geoblinkDesignSystem_inferPageSizeMixin_inferredPageSize'
+}
+
+export const INFERRED_PAGE_SIZE_CHANGED_EVENT_NAME = 'inferred-page-size-changed'
+
+/**
+ * @callback InferPageSizeStepCallback
+ * @return Promise<void>
+ */
+
+/**
+ * @callback GetHeight
+ * @return number
+ */
+
+/**
+ * @callback NextTick
+ * @param {Function} [callback]
+ * @return Promise<void>
+ */
+
+/**
+ * @callback Emit
+ * @param {string} name
+ * @param {any} payload
+ */
+
+/**
+ * @typedef {object} InferPageSizeParams
+ * @property {number} sourceDataLength Total amount of data that can be provided
+ * at most, ignoring any pagination constraint
+ * @property {InferPageSizeStepCallback} [params.before] Function to be
+ * called immediately before starting iteration of the algorithm but after
+ * running any pending tick
+ * @property {InferPageSizeStepCallback} [params.after] Function to be
+ * called immediately after each iteration of the algorithm but before running
+ * any pending tick
+ * @property {InferPageSizeStepCallback} [params.beforeEach] Function to be
+ * called before each iteration of the algorithm.
+ * @property {InferPageSizeStepCallback} [params.afterEach] Function to be
+ * called after each iteration of the algorithm.
+ * @property {GetHeight} getContainerHeight Function returning the height of the
+ * container where content is displayed. It should be smaller than content
+ * height when scroll is required and greater than content height when there's
+ * empty unused space.
+ * @property {GetHeight} getContentHeight Function returning the height of the
+ * actual content displayed.
+ */
+
+/**
+ * @callback InferPageSize
+ * @param {InferPageSizeParams} [params]
+ * @return {Promise<void>|null}
+ */
+
+/**
+ * @typedef {object} ComponentWithInferPageSizeMixin
+ * @property {boolean} geoblinkDesignSystem_inferPageSizeMixin_isInferringPageSize
+ * @property {number} geoblinkDesignSystem_inferPageSizeMixin_internallyForcedCurrentPageStart
+ * @property {number|null} geoblinkDesignSystem_inferPageSizeMixin_inferredPageSize
+ * @property {InferPageSize} inferPageSize
+ * @property {NextTick} $nextTick
+ * @property {Emit} $emit
+ */
+
+/**
+ * @mixin
+ */
+export default {
+  data () {
+    return {
+      // Used to force table to display first page during page size inference
+      [DATA_KEYS.internallyForcedCurrentPageStart]: -1,
+      [DATA_KEYS.inferredPageSize]: null
+      // This is intentionally non-reactive to avoid triggering infinite loops
+      // when inferring page size stops
+      // [DATA_KEYS.isInferringPageSize]: false
+    }
+  },
+  methods: {
+    /**
+     * @param {InferPageSizeParams} params
+     */
+    inferPageSize (params) {
+      const vm = (/** @type ComponentWithInferPageSizeMixin */ (/** @type unknown */ this))
+      if (vm[DATA_KEYS.isInferringPageSize]) return null
+
+      vm[DATA_KEYS.isInferringPageSize] = true
+      vm[DATA_KEYS.internallyForcedCurrentPageStart] = 0
+      vm[DATA_KEYS.inferredPageSize] = 1
+
+      return vm.$nextTick()
+        .then(() => params && params.before ? params.before() : null)
+        .then(() => attemptToIncreaseInferredPageSize(params, vm))
+        .then(() => params && params.after ? params.after() : null)
+        .then(() => {
+          vm[DATA_KEYS.internallyForcedCurrentPageStart] = -1
+          vm.$nextTick()
+        })
+        .then(() => {
+          vm[DATA_KEYS.isInferringPageSize] = false
+
+          /**
+           * Inferred page size changed.
+           *
+           * @event inferred-page-size-changed
+           * @type {number}
+           */
+          vm.$emit(INFERRED_PAGE_SIZE_CHANGED_EVENT_NAME, vm[DATA_KEYS.inferredPageSize])
+        })
+    }
+  }
+}
+
+/**
+ * @param {InferPageSizeParams} params
+ * @param {ComponentWithInferPageSizeMixin} vm
+ */
+function attemptToIncreaseInferredPageSize (params, vm) {
+  if (vm[DATA_KEYS.inferredPageSize] >= params.sourceDataLength) return
+
+  if (params.beforeEach) params.beforeEach()
+
+  vm[DATA_KEYS.inferredPageSize] += 1
+
+  return vm.$nextTick().then(function () {
+    if (params.afterEach) params.afterEach()
+
+    const containerHeight = params.getContainerHeight()
+    const contentHeight = params.getContentHeight()
+
+    const isVerticalScrollRequired = containerHeight < contentHeight
+
+    if (isVerticalScrollRequired) {
+      vm[DATA_KEYS.inferredPageSize] -= 1
+      return vm.$nextTick()
+    }
+
+    return attemptToIncreaseInferredPageSize(params, vm)
+  })
+}
