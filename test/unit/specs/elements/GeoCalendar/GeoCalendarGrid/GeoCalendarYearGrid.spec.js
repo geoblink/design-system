5e7df60910c27d4c9fcfb22c4ecf1973de26e4d9
Ricardo Boluda <rboluda@geoblink.com>
2019-07-26 14:48:55 +0200

    Add basic structure for all geoCalendar tests

---
+++ test/unit/specs/elements/GeoCalendar/GeoCalendarGrid/GeoCalendarYearGrid.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import GeoCalendar from '@/elements/GeoCalendar/GeoCalendarGrid/GeoCalendarYearGrid.vue'
+
+describe('GeoCalendar', () => {
+  it('should render', function () {
+    const wrapper = mount(GeoCalendar)
+    expect(wrapper.find('.geo-calendar').exists()).toBe(true)
+  })
+})
