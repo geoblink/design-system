ce2fab0e95db45d9f29e33a5ede07190b2a0265d
Solène Gardies <sgardies@geoblink.com>
2019-09-17 15:28:44 +0200

    added test for geoborderedbox

---
+++ test/unit/specs/elements/GeoBorderedBox/GeoBorderedBox.spec.js
@@ -0,0 +1,32 @@
+import _ from 'lodash'
+import { createLocalVue, mount } from '@vue/test-utils'
+import GeoBorderedBox from '@/elements/GeoBorderedBox/GeoBorderedBox.vue'
+
+// create an extended `Vue` constructor
+const localVue = createLocalVue()
+localVue.component('geo-bordered-box', GeoBorderedBox)
+
+describe('GeoBorderedBox', () => {
+  it('Should render alert component', function () {
+    const wrapper = mount(GeoBorderedBox, {})
+    expect(wrapper.find('.geo-bordered-box').exists()).toBe(true)
+  })
+  
+  it('Should render default slot', function () {
+    const wrapper = mount(GeoBorderedBox, {
+      slots: {
+        default: [`<span class="my-demo-content">Just some unique demo content</span>`]
+      }
+    })
+    expect(wrapper.find('.my-demo-content').exists()).toBe(true)
+  })
+  
+  it('Should apply a CSS suffix when the modifier is provided', function () {
+    const wrapper = mount(GeoBorderedBox, {
+        propsData: {
+          cssModifier: 'test'
+        }
+    })
+    expect(wrapper.find('.geo-bordered-box--test').exists()).toBe(true)
+  })
+})
