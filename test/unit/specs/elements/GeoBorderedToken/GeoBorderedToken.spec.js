83456e06f332b6b2252a9532e9742d8f4eff3197
Solène Gardies <sgardies@geoblink.com>
2019-09-18 14:19:03 +0200

    updated icon tests in geoborderedtoken

--- test/unit/specs/elements/GeoBorderedToken/GeoBorderedToken.spec.js
+++ test/unit/specs/elements/GeoBorderedToken/GeoBorderedToken.spec.js
@@ -6,6 +6,11 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fas)
 
+const FontAwesomeIconMock = {
+  props: ['icon'],
+  template: '<div></div>'
+}
+
 // create an extended `Vue` constructor
 const localVue = createLocalVue()
 localVue.component('geo-bordered-token', GeoBorderedToken)
@@ -29,17 +34,30 @@ describe('GeoBorderedToken', () => {
     expect(wrapper.find('.geo-bordered-token__label').text()).toBe('test')
   })
 
-  it('Should display an icon when provided', function () {
+  it('Should display the correct icon when provided', function () {
     const wrapper = mount(GeoBorderedToken, {
       propsData: {
         label: 'test',
         icon: ['fas', 'map-marker']
       },
       stubs: {
+        'font-awesome-icon': FontAwesomeIconMock
+      }
+    })
+    const fontAwesomeIconElem = wrapper.find(FontAwesomeIconMock)
+    expect(fontAwesomeIconElem.props().icon).toStrictEqual(['fas', 'map-marker'])
+  })
+
+  it('Should not display any icon when none provided', function () {
+    const wrapper = mount(GeoBorderedToken, {
+      propsData: {
+        label: 'test'
+      },
+      stubs: {
         'font-awesome-icon': FontAwesomeIcon
       }
     })
-    expect(wrapper.find('.fa-map-marker').exists()).toBe(true)
+    expect(wrapper.find('.geo-bordered-token__icon').exists()).toBe(false)
   })
 
   it('Should apply a CSS suffix when the modifier is provided', function () {
