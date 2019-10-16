bec6edd8b3d3070ae10be04f0f3f227c4d821b2d
Solène Gardies <sgardies@geoblink.com>
2019-10-16 09:42:02 +0200

    put click on tertiary button

--- src/elements/GeoList/GeoListFooterButton.vue
+++ src/elements/GeoList/GeoListFooterButton.vue
@@ -2,9 +2,10 @@
   <div :class="`geo-list-footer-button${cssSuffix}`">
     <div
       class="geo-list-footer-button__button"
-      @click="handleClick($event)"
     >
-      <geo-tertiary-button>
+      <geo-tertiary-button
+        @click="handleClick($event)"
+      >
         <!-- @slot Use this slot to customize the label of the button -->
         <slot />
       </geo-tertiary-button>
