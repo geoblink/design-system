fce343293f2cc15ec00656be602837fb2e777e7c
David Del Prado <ddelprado@geoblink.com>
2019-11-07 15:20:39 +0100

    Revert "component improvements to use data"
    This reverts commit f21965c969d2e141e333cf348313f7e55abe40a6.

--- src/elements/GeoTrimmedContent/GeoTrimmedContent.vue
+++ src/elements/GeoTrimmedContent/GeoTrimmedContent.vue
@@ -73,14 +73,15 @@ export default {
       if (!this.$refs.content) return
 
       this.tooltipHTML = this.$refs.content.innerHTML
-      this.htmlElement.innerHTML = this.tooltipHTML
+      const existingElement = document.getElementById(this.idTooltipContentNode)
+      existingElement.innerHTML = this.tooltipHTML
     },
 
     addTooltipHTMLContentNode () {
-      if (this.htmlElement) return
+      const element = document.getElementById(this.idTooltipContentNode)
+      if (element) return
 
       const newElement = document.createElement('div')
-      this.htmlElement = newElement
       newElement.setAttribute('id', this.idTooltipContentNode)
       document.body.appendChild(newElement)
     },
@@ -88,7 +89,8 @@ export default {
     removeTooltipHTMLContentNode () {
       if (!this.isContentTrimmed) return
 
-      if (this.htmlElement) this.htmlElement.parentNode.removeChild(this.htmlElement)
+      const element = document.getElementById(this.idTooltipContentNode)
+      if (element) element.parentNode.removeChild(element)
     }
   },
   render (createElement) {
