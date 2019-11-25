f9698f348c8b4e24cf5fe0208e01fedd1c3ff5df
Ricardo Boluda <rboluda@geoblink.com>
2019-11-25 17:29:37 +0100

    Add tests for sidebar and wizard

---
+++ test/unit/specs/elements/GeoSidebarLayout/GeoSidebarLayout.spec.js
@@ -0,0 +1,34 @@
+import { mount } from '@vue/test-utils'
+import GeoSidebarLayout from '@/elements/GeoSidebarLayout/GeoSidebarLayout.vue'
+
+describe('GeoSidebarLayout', function () {
+  it('Should render GeoSidebarLayout component', function () {
+    const wrapper = mount(GeoSidebarLayout, {
+      stubs: ['geo-vertical-layout']
+    })
+    expect(wrapper.find('.geo-sidebar-layout').exists()).toBe(true)
+  })
+
+  it('Should render default slot', function () {
+    const wrapper = mount(GeoSidebarLayout, {
+      stubs: ['geo-vertical-layout'],
+      slots: {
+        default: [`<span class="my-demo-content">Just some unique demo content</span>`]
+      }
+    })
+    expect(wrapper.find('.my-demo-content').exists()).toBe(true)
+  })
+
+  it('Should render header and footer slots', function () {
+    const wrapper = mount(GeoSidebarLayout, {
+      stubs: ['geo-vertical-layout'],
+      slots: {
+        default: [`<span class="my-demo-content">Just some unique demo content</span>`],
+        header: [`<span class="my-header-demo-content">Just some unique header demo content</span>`],
+        footer: [`<span class="my-footer-demo-content">Just some unique footer demo content</span>`]
+      }
+    })
+    expect(wrapper.find('.my-header-demo-content').exists()).toBe(true)
+    expect(wrapper.find('.my-footer-demo-content').exists()).toBe(true)
+  })
+})
