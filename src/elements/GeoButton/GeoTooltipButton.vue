086b895a4469f41679691b76d1dfb7db44b4dd87
Ricardo Boluda <rboluda@geoblink.com>
2019-11-29 14:44:16 +0100

    v28.1.0-beta.1

---
+++ src/elements/GeoButton/GeoTooltipButton.vue
@@ -0,0 +1,17 @@
+<template src="./GeoButton.template.html" />
+
+<script>
+import mixin, { TYPES } from './GeoButton.mixin'
+
+export default {
+  name: 'GeoTooltipButton',
+  status: 'ready',
+  release: '28.1.0',
+  mixins: [mixin],
+  computed: {
+    type () {
+      return TYPES.tooltip
+    }
+  }
+}
+</script>
