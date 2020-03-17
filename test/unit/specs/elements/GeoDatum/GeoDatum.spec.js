7cd471c11e7c40827df343eea702f4fc0ba28558
Carlos Navas <cnavas@geoblink.com>
2020-03-17 10:44:20 +0100

    Remove GeoDatum

--- test/unit/specs/elements/GeoDatum/GeoDatum.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import GeoDatum from '@/elements/GeoDatum/GeoDatum.vue'
-
-describe('GeoDatum', function () {
-  it('Should render', function () {
-    const wrapper = mount(GeoDatum)
-    expect(wrapper.find('.geo-datum').exists()).toBe(true)
-  })
-})
