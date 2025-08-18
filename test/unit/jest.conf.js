e785e158e1e16ecfe3f10c22177dddc09d1d9a48
David Letrán <dletran@mytraffic.fr>
2025-08-18 18:53:52 +0200

    update jest config

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -4,16 +4,24 @@ module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
   modulePaths: ['<rootDir>'],
   moduleFileExtensions: ['js', 'json', 'vue'],
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@vue/test-utils': '<rootDir>/node_modules/@vue/test-utils/dist/vue-test-utils.cjs.js',
+    '^vue-template-compiler$': '<rootDir>/node_modules/@vue/compiler-sfc'
   },
   transform: {
     '.*\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.vue$': '<rootDir>/node_modules/vue-jest'
+    '.*\\.vue$': ['<rootDir>/node_modules/@vue/vue3-jest', {
+      compilerOptions: {
+        compatConfig: {
+          MODE: 2
+        }
+      }
+    }]
   },
   testResultsProcessor: '<rootDir>/node_modules/jest-junit-reporter',
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  setupFiles: ['<rootDir>/test/unit/setup'],
   coverageDirectory: '<rootDir>/test/unit/coverage',
   coverageReporters: ['json', 'text', 'html', 'lcov'],
   collectCoverageFrom: [
